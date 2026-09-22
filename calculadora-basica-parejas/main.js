832b7085193e9395a6794b09968de24953f37c10
Isaac Razo <197133338+ChanisBlu@users.noreply.github.com>
2026-09-22 12:41:32 -0600

    calculadora basica en parejas, funconando en consola de navegador

---
+++ calculadora-basica-parejas/main.js
@@ -0,0 +1,9 @@
+(function() {
+  let numero1 = Number(prompt("Escribe el primer número"));
+  let numero2 = Number(prompt("Escribe el segundo número"));
+
+  console.log("Suma = ", numero1 + numero2);
+  console.log("Resta = ", numero1 - numero2);
+  console.log("Multiplicación = ", numero1 * numero2);
+  console.log("División = ", numero1 / numero2);
+})(); 
